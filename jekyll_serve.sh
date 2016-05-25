0a6ec86165ee517de09d7f2f375fcd098534ce1b
zhangchao <wszc1993@live.com>
2016-05-25 23:11:35 +0800

    修改启动脚本jekyll.sh

--- jekyll_serve.sh
+++ jekyll_serve.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
-
+#!/bin/bash
+tid=$(ps -ef |grep jekyll |grep -v 'grep'|awk '{print $2}')
+for i in ${tid[@]};do
+    echo $i
+    kill -9 $i
+    echo "KILLING JEKYLL"
+    sleep 5
+done
 rm -rf _site/*
 jekyll serve -H 114.215.83.238 -B
